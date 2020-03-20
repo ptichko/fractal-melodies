b51c9b86bcb7fcc653623284c03cfe5eaf1d85f1
Parker <parker.tichko@gmail.com>
2020-03-20 14:38:58 -0400

    Updated MATLAB script, added example .mat files, and MIDI files

--- MakeF0MIDISequence.m
+++ MakeF0MIDISequence.m
@@ -2,24 +2,31 @@
 %% Make (fractal) MIDI melodies sequences from R time series
 
 %Directory of time series
-addpath('C:\Users\Parker\Dropbox\1fNoiseSchaffer\FractalClicks\F0_Fractal\');
+files = dir('D:\R\Fractal Melodies\fractal-melodies\MAT files\*.mat');
 
-%Load in time series
-load H0.75.mat %load in time series of MIDI note values from R
-
-%Create sequence
+% Create sequence
 seq = [];
-for x = 1:length(TS.y_MP)
-    note = createnmat(TS.y_MP(x)); 
-    note = setvalues(note,'onset',(1*x)); %start onset in beats
-    note = setvalues(note,'dur',1); %note duration in beats
-%     note = setvalues(note,'onset',(0.5*x),'sec'); %start onset in sec
-%     note = setvalues(note,'dur',0.5,'sec'); %note duration in sec
-    seq = [seq; note];
+
+for ii = 1: length(files)
     
+    % load in time series
+    load(files(ii).name)
+    
+    for x = 1:length(TS.y_MP)
+        note = createnmat(TS.y_MP(x)); 
+        note = setvalues(note,'onset',(x-1)); % start onset in beats
+        note = setvalues(note,'dur',1); % note duration in beats
+    %     note = setvalues(note,'onset',(0.5*x),'sec'); %start onset in sec
+    %     note = setvalues(note,'dur',0.5,'sec'); %note duration in sec
+        seq = [seq; note];
+        
+    end
+    
+    % Write MIDI file
+    writemidi(seq,strcat(files(ii).name(1:end-4), '.mid'), 120);
+        
+    % Reset sequence
+    seq = [];
+
 end
-
-%Write MIDI file
-writemidi(seq,'testmelody1.mid',100);
-
 
